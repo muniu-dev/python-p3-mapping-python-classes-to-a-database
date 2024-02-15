676a14ddcf94c17145cea7ca882d1c4c0124dad7
Michael Muniu <muniu.dev@gmail.com>
2024-02-15 20:47:34 +0300

    Lab complete

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,37 @@
 from config import CONN, CURSOR
 
 class Song:
-    pass
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(cls):
+        # Create the 'songs' table
+        CURSOR.execute('''
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        ''')
+
+    def save(self, connection=CONN):
+        # Save the song to the 'songs' table
+        with connection:
+            cursor = connection.cursor()
+            cursor.execute(
+            'INSERT INTO songs (name, album) VALUES (?, ?)',
+            (self.name, self.album)
+        )
+
+        # Set the id using the last inserted row id
+            self.id = cursor.lastrowid
+
+    @classmethod
+    def create(cls, name, album, connection=CONN):
+        # Create and save a new song, then return the Song instance
+        song = cls(name, album)
+        song.save(connection)
+        return song
